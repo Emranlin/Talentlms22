750987319451d67dc82aecf084dca2ee5d76017c
Emranlin <117289842+Emranlin@users.noreply.github.com>
2022-12-02 01:37:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        ArrayList<Integer> number = new ArrayList<Integer>();
+        ArrayList<Integer>oddNumber=new ArrayList<Integer>();
+        ArrayList<Integer>evenNumber=new ArrayList<Integer>();
+        for (int i = 0; i <20 ; i++) {
+            int array= random.nextInt(10,50);
+            number.add(array);
+                if(array%2==0){
+                oddNumber.add(array);
+            }else{
+                evenNumber.add(array);
+            }
+        }
+            System.out.println("There are all number: "+number);
+            System.out.println("There are odd number: "+oddNumber);
+            System.out.println("There are even number: "+evenNumber);
+
+
+        }
+    }
+
